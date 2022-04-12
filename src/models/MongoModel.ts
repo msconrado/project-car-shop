e59c8ef9e1c83a503673a008a505fafa9d400674
Matheus <ms.conrado25@hotmail.com>
2022-04-11 23:32:04 -0300

    criando o arquivo MongoModel generico

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,21 @@
+import { Model as MModel, Document } from 'mongoose';
+import { Model as IModel } from '../interfaces/ModelInterface';
+
+class MongoModel<T> implements IModel<T> {
+  constructor(protected model: MModel<T & Document>) {}
+
+  create = async (obj: T): Promise<T> => this.model.create({ ...obj });
+
+  read = async (): Promise<T[]> => this.model.find();
+
+  readOne = async (id: string): Promise<T | null> =>
+    this.model.findOne({ _id: id });
+
+  update = async (id: string, obj: T): Promise<T | null> =>
+    this.model.findByIdAndUpdate({ _id: id }, { ...obj });
+
+  delete = async (id: string): Promise<T | null> =>
+    this.model.findOneAndDelete({ _id: id });
+}
+
+export default MongoModel;
