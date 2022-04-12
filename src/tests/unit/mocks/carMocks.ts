e25a028162bb5745823b6e7c3953fc4560a0ffd1
Matheus <ms.conrado25@hotmail.com>
2022-04-12 20:17:52 -0300

    criando testes para as funções read e readOne

--- src/tests/unit/mocks/carMocks.ts
+++ src/tests/unit/mocks/carMocks.ts
@@ -1,5 +1,5 @@
 export const carIdCreateMock = {
-  _id: '1',
+  _id: '6255f38761dc2797fbbd5495',
   model: 'Ferrari Maranello',
   year: 1963,
   color: 'red',
@@ -16,3 +16,13 @@ export const carCreateMock = {
   seatsQty: 2,
   doorsQty: 2,
 };
+
+export const carIdFindMock = [{
+  _id: '6255f38761dc2797fbbd5495',
+  model: 'Ferrari Maranello',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+}];
--- src/tests/unit/models/model.test.ts
+++ src/tests/unit/models/model.test.ts
@@ -1,23 +1,69 @@
-import { carIdCreateMock } from './../mocks/carMocks';
+import { carIdFindMock } from './../mocks/carMocks';
+import { carIdCreateMock } from '../mocks/carMocks';
 import { expect } from 'chai';
 import * as sinon from 'sinon';
 import CarModels from '../../../models/CarModels';
+import { Car } from '../../../interfaces/CarInterface';
 
-describe('Car Models, rota /cars', () => {
+interface CarId {
+  _id: string;
+  model: string;
+  year: number;
+  color: string;
+  buyValue: number;
+  seatsQty: number;
+  doorsQty: number;
+}
+describe('Car Models', () => {
   let carModels = new CarModels();
+  describe('rota POST /cars', () => {
+    describe('Create', () => {
+      before(async () => {
+        sinon.stub(carModels.model, 'create').resolves(carIdCreateMock);
+      });
 
-  describe('Create', () => {
-    before(async () => {
-      sinon.stub(carModels.model, 'create').resolves(carIdCreateMock);
+      after(() => {
+        sinon.restore();
+      });
+
+      it('deve retornar um objeto de Car', async () => {
+        const car = await carModels.create(carIdCreateMock);
+        expect(car).to.deep.equal(carIdCreateMock);
+      });
     });
+  });
 
-    after(() => {
-      sinon.restore();
+  describe(' rota GET /cars', () => {
+    describe('Read', () => {
+      before(async () => {
+        sinon.stub(carModels.model, 'find').resolves(carIdFindMock as any[]);
+      });
+
+      after(() => {
+        sinon.restore();
+      });
+
+      it('deve retornar um array de Cars', async () => {
+        const cars = await carModels.read();
+        expect(cars).to.deep.equal(carIdFindMock);
+      });
     });
+  });
 
-    it('deve retornar um objeto de Car', async () => {
-      const car = await carModels.create(carIdCreateMock);
-      expect(car).to.deep.equal(carIdCreateMock);
+  describe(' rota GET /cars/:id', () => {
+    describe('ReadOne', () => {
+      before(async () => {
+        sinon.stub(carModels.model, 'findOne').resolves(carIdCreateMock as any);
+      });
+
+      after(() => {
+        sinon.restore();
+      });
+
+      it('deve retornar um objeto de Cars', async () => {
+        const cars = await carModels.readOne('6255f38761dc2797fbbd5495');
+        expect(cars).to.deep.equal(carIdCreateMock);
+      });
     });
   });
 });
