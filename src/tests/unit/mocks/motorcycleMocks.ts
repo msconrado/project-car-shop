0f57cbde07ecf6a7c351dba351e2d1384dc48446
Matheus <ms.conrado25@hotmail.com>
2022-04-14 14:08:49 -0300

    criando testes para o create do motorcycleControllers

---
+++ src/tests/unit/mocks/motorcycleMocks.ts
@@ -0,0 +1,18 @@
+export const motorcycleIdCreateMock = {
+  _id: '6255f38761dc2797fbbd5495',
+  model: 'Ferrari Maranello',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+};
+
+export const motorcycleCreateMock = {
+  model: 'Ferrari Maranello',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+};
